5fe56f39a721ce0b8cc233bce2e9577122e5f62a
Zubair Shahid <zubairshahid814@gmail.com>
2022-11-26 10:27:40 +0500

    message update

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const { string, any } = require('joi');
 const Joi = require('joi');
 
 const body = {
-  username: 'test@gmail.com', password: 'sds', name: 12, age: '23'
+  username: 'test@gmail.com', password: 'sds', name: '12', age: '23',
 };
 
 const auth = {
   username: null,
   password: null,
+  age: null,
   name: null,
   ...body,
 }
@@ -15,13 +16,12 @@ const auth = {
 const customWarningFunc = (type) => (value, helper) => {
   if (value) {
     if (typeof (value) != type) {
-      // helper.warn('any.unknown', { w: helper.state.path[0] });
-      helper.warn('any.unknown');
+      helper.warn('any.unknown', { msg: `${helper.state.path[0]} must be ${type}` });
       return
     }
     return value;
   }
-  helper.warn('any.unknown')
+  helper.warn('any.unknown', { msg: `${helper.state.path[0]} must be not be empty` });
 }
 
 
@@ -30,7 +30,8 @@ const authSchema = Joi.object()
     username: Joi.string().required(),
     password: Joi.string().required(),
     name: Joi.custom(customWarningFunc('string')),
-    age: Joi.custom(customWarningFunc('number')),
+    // age: Joi.custom(customWarningFunc('number')),
+    age: Joi.number().min(10).custom(customWarningFunc('number')),
   })
   .options({
     abortEarly: false,
