4029b95333aac7313c7616ca59c44cbe3e2a4063
Zubair Shahid <zubairshahid814@gmail.com>
2022-11-27 14:01:13 +0500

    Final Proposal

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const Joi = require('joi');
 
 //This is the object we will be getting from DB/Client
 const auth = {
-  username: 'test@gmail.com', password: 'sds', name: '12', age: '23', phone: true
+  username: 'test@gmail.com', password: 'sds', name: '12', age: '23', phone: '22', city: '12',
 };
 
-//This will be our current Validation - Don't have to change anything
 const authSchema = Joi.object()
   .keys({
     username: Joi.string().required(),
@@ -18,70 +17,23 @@ const authSchema = Joi.object()
     gender: Joi.string(),
     hobbies: Joi.string(),
     city: Joi.string(),
+    gos: Joi.string(),
   })
   .options({
     abortEarly: false,
     allowUnknown: false,
-  });
+  }).custom((value, helper) => { //just have to attach this functions
+    const undefinedKeys = [];
+    const schemaKeys = helper.schema.$_terms.keys;
+    schemaKeys.map((schemaKey) => {
+      if (!value[schemaKey.key]) {
+        undefinedKeys.push(schemaKey.key);
+      }
+    })
+    helper.warn('any.unknown', { msg: 'Values Required', value: undefinedKeys })
+  }, "Warn on undefined keys");
 
 
-const { value, error } = authSchema.validate(auth);
-
-//If error found, we will not go to the next MD. 
-console.log(error);
-
-
-//Here our Current MD end for Validation and we have an Object which passed our First Validation Check 
-
-//We Start our 2nd MD here to get warning for the OptionalFields() - It will be a seperate file and called after the first MD
-
-//This object will contain all the fields with null values
-//The Body will now again go into this check -> This will only give warning for the fields which are not present
-const auth2 = {
-  username: null,
-  password: null,
-  age: null,
-  name: null,
-  gender: null,
-  country: null,
-  hobbies: null,
-  city: null,
-  phone: null,
-  ...value //combining the object here
-}
-
-const customWarningFunc = (value, helper) => {
-  if (value) return value;
-  helper.warn('any.unknown', { msg: `${helper.state.path[0]} must be not be empty` });
-}
-
-//In this validation every field must have a customfunc attached
-const authSchema2 = Joi.object()
-  .keys({
-    username: Joi.custom(customWarningFunc),
-    password: Joi.custom(customWarningFunc),
-    name: Joi.custom(customWarningFunc),
-    age: Joi.custom(customWarningFunc),
-    gender: Joi.custom(customWarningFunc),
-    country: Joi.custom(customWarningFunc),
-    hobbies: Joi.custom(customWarningFunc),
-    city: Joi.custom(customWarningFunc),
-    phone: Joi.custom(customWarningFunc),
-  }).options({
-    abortEarly: false,
-    allowUnknown: false,
-  });
-
-
-const { warning } = authSchema2.validate(auth2);
-
-//Here we have the warnings now, we can store them in a seperate object or do whatever we want to do with them
-console.log(warning)
-
-
-
-
-
-
-
-
+const { value, error, warning } = authSchema.validate(auth);
+console.log("error", error);
+console.log("warning", warning.details[0].context);
